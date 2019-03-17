e61c6842b587b4a7355bb3bd7efb5c4f1a4a774b
LiawKC <40540470+LiawKC@users.noreply.github.com>
2019-03-17 17:32:11 +0800

    Update GC12.py

--- GC12.py
+++ GC12.py
@@ -7,57 +7,7 @@ TimePerPerson = TotalTime/TotalPeople
 RoundedHoursPerPerson = int(round(TimePerPerson))
 RoundedMinutesPerPerson = RoundedHoursPerPerson*60
 
-if TotalPeople == 1:
+for i in range(TotalPeople):
     for i in range (RoundedMinutesPerPerson, 0, -1):
         print("You have", i)
-        time.sleep(0.1)
-
-if TotalPeople == 2:
-    for i in range (RoundedMinutesPerPerson, 0, -1):
-        print("The first person has", i)
-        time.sleep(0.1)
-    for i in range (RoundedMinutesPerPerson, 0, -1):
-        print("The second person has", i)
-        time.sleep(0.1)
-
-if TotalPeople == 3:
-    for i in range (RoundedMinutesPerPerson, 0, -1):
-        print("The first person has", i)
-        time.sleep(0.1)
-    for i in range (RoundedMinutesPerPerson, 0, -1):
-        print("The second person has", i)
-        time.sleep(0.1)
-    for i in range (RoundedMinutesPerPerson, 0, -1):
-        print("The third person has", i )
-        time.sleep(0.1)
-
-if TotalPeople == 4:
-    for i in range(RoundedMinutesPerPerson, 0, -1):
-        print("The first person has", i)
-        time.sleep(0.1)
-    for i in range(RoundedMinutesPerPerson, 0, -1):
-        print("The second person has", i)
-        time.sleep(0.1)
-    for i in range(RoundedMinutesPerPerson, 0, -1):
-        print("The third person has", i)
-        time.sleep(0.1)
-    for i in range(RoundedMinutesPerPerson, 0, -1):
-        print("The fourth person has", i)
-        time.sleep(0.1)
-
-if TotalPeople == 5:
-    for i in range(RoundedMinutesPerPerson, 0, -1):
-        print("The first person has", i)
-        time.sleep(0.1)
-    for i in range(RoundedMinutesPerPerson, 0, -1):
-        print("The second person has", i)
-        time.sleep(0.1)
-    for i in range(RoundedMinutesPerPerson, 0, -1):
-        print("The third person has", i)
-        time.sleep(0.1)
-    for i in range(RoundedMinutesPerPerson, 0, -1):
-        print("The fourth person has")
-        time.sleep(0.1)
-    for i in range(RoundedMinutesPerPerson, 0, -1):
-        print("The fifth person has", i)
         time.sleep(0.1)
