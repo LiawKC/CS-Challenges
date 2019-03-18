5c42cbdfc219436500166d366873b06ed447c456
LiawKC <40540470+LiawKC@users.noreply.github.com>
2019-03-18 21:32:06 +0800

    Add files via upload

--- TU14.py
+++ TU14.py
@@ -1,7 +1,19 @@
-MyString = "abcdef"
-print(MyString[0:3])
-print(MyString[3::])
-print(MyString[0:6:2])
-print(MyString[0:-2])
-print(MyString[-1:-7:-1])
-print(MyString[0:-2])
+import tkinter
+import random
+
+window = tkinter.Tk()
+
+def RandomNumber():
+    MyRandom = random.randint(1, 6)
+    dice_thrown.configure(text="Dice thrown: " + str(MyRandom))
+
+MyTitle = tkinter.Label(window, text="Random Number Generator",font="Helvetica 16 bold")
+MyTitle.pack()
+
+MyButton = tkinter.Button(window, text="OK", command=RandomNumber)
+MyButton.pack()
+
+dice_thrown = tkinter.Label(window, font="Helvetica 16 bold")
+dice_thrown.pack()
+
+window.mainloop()
