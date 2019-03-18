105e2dffcf9d62b4c124115ac164ad22e8f6b5d3
LiawKC <40540470+LiawKC@users.noreply.github.com>
2019-03-18 18:13:11 +0800

    Add files via upload

---
+++ AS71.py
@@ -0,0 +1,25 @@
+Sentence = input("What do you want to cipher? ")
+
+NumberOfLetters = len(Sentence)
+
+x = " "
+
+for Shift in range(26):
+    for i in range(NumberOfLetters):
+        NumberConverted = ord(Sentence[i])
+        NumberShifted = NumberConverted + Shift
+
+        if NumberShifted > 90 and NumberShifted < 97:
+            NumberShifted = NumberShifted - 26
+        if NumberShifted > 122:
+            NumberShifted = NumberShifted - 26
+        if NumberConverted == 32:
+            NumberShifted = NumberShifted - Shift
+
+        if NumberShifted > 57 and NumberShifted < 65:
+            NumberShifted = NumberShifted - 9
+
+        LetterConverted = chr(NumberShifted)
+        x = x + LetterConverted
+    print(x)
+    x = ""
