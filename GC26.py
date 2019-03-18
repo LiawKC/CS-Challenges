0775fc64024cb5a9a40e72d35aeac2544cc07381
LiawKC <40540470+LiawKC@users.noreply.github.com>
2019-03-18 17:40:37 +0800

    Add files via upload

---
+++ GC26.py
@@ -0,0 +1,11 @@
+print("Welcome to the drunkometer, my friend")
+
+print("")
+
+Phrase = input("Type a phrase: ")
+Phrase2 = input("Type the exact same phrase again: ")
+
+if Phrase == Phrase2:
+    print("You have passed the drunkometer. Welcome.")
+else:
+    print("You're fired!")
