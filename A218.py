c41baff38de9ce0ce297090e3a344e7840cde2b0
LiawKC <40540470+LiawKC@users.noreply.github.com>
2019-03-18 22:21:25 +0800

    Add files via upload

---
+++ A218.py
@@ -0,0 +1,10 @@
+List =[]
+
+NumberOFfValues = int(input("How many triangular numbers do you want? "))
+
+for i in range(NumberOFfValues):
+    Number = (0.5*i) * (i+1)
+    List.append(Number)
+
+print(List)
+
